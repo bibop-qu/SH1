c7262bd0aee2dd2ca6f0e56d46f60e8d20d42a04
Bastien LE-QUERE-DUMONT <basle-qu@e1r4p11.42.fr>
2015-05-21 16:33:53 +0200

    ft_minishell

--- srcs/exec.c
+++ srcs/exec.c
@@ -1,6 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: basle-qu <basle-qu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/05/21 13:40:06 by basle-qu          #+#    #+#             */
+/*   Updated: 2015/05/21 16:26:58 by basle-qu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 #include "tools.h"
+#include "tools_env.h"
+
+char	*ft_point(char **e)
+{
+	int		i;
+	char	*result;
+	char	**name;
+	char	**path;
+
+	i = 0;
+	result = NULL;
+	name = recup_name(e);
+	while (name[i] && ft_strcmp(name[i], "PWD"))
+		i++;
+	path = ft_strsplit(e[i], '=');
+	result = ft_strjoin(ft_strdup(path[1]), "/");
+	free_tab(name);
+	free_tab(path);
+	return (result);
+
+}
 
 char	**recup_path(char **e)
 {
@@ -40,8 +72,16 @@ void	do_fork(char **cmd, char **e, char **path)
 		}
 		while (path[i])
 		{
-			tmp = ft_strjoin(path[i], "/");
-			bin = ft_strjoin(tmp, cmd[0]);
+			if (!ft_strchr(cmd[0], '.'))
+			{
+				tmp = ft_strjoin(path[i], "/");
+				bin = ft_strjoin(tmp, cmd[0]);
+			}
+			else
+			{
+				tmp = ft_point(e);
+				bin = ft_strjoin(tmp, cmd[0] + 2);
+			}
 			free(tmp);
 			if (execve(bin, cmd, e) == -1 && path[i])
 				i++;
@@ -49,6 +89,7 @@ void	do_fork(char **cmd, char **e, char **path)
 			{
 				write(2, cmd[0], ft_strlen(cmd[0]));
 				write(2, ": command not found\n", 20);
+				free(bin);
 				exit(1);
 			}
 		}
