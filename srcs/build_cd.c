daeece01e4897248bf9b8692fdb13fd82c5ee819
Bastien LE-QUERE-DUMONT <basle-qu@e1r12p8.42.fr>
2015-05-29 14:15:41 +0200

    approche fin

--- srcs/build_cd.c
+++ srcs/build_cd.c
@@ -1,40 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   build_cd.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: basle-qu <basle-qu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/05/29 10:44:03 by basle-qu          #+#    #+#             */
+/*   Updated: 2015/05/29 14:11:25 by basle-qu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 #include "tools.h"
 #include "tools_env.h"
+#include "env.h"
 
 char	**ft_cd(char **e, char **cmd)
 {
-	int		i;
-	char	*dir;
-	char	*tmp;
-	char	*tmp2;
-	char	**name;
+	char	**tmp;
 
-	i = 0;
-	name = recup_name(e);
-	dir = NULL;
-	while (ft_strcmp("PWD", name[i]))
-		i++;
-	dir = getcwd(dir,ft_strlen(e[i]));
-	free(e[i]);
-	tmp = ft_strdup(cmd[1]);
-	tmp2 = ft_strjoin("PWD=", tmp);
-	e[i] = ft_strdup(tmp2);
-	i = 0;
-	while (ft_strcmp("OLDPWD", name[i]))
-		i++;
+	tmp = (char**)malloc(sizeof(char*) * 4);
+	tmp[0] = ft_strdup("toto");
+	tmp[1] = ft_strdup("OLDPWD");
+	tmp[2] = getcwd(tmp[0], 256);
+	tmp[3] = NULL;
+	e = ft_setenv(e, tmp);
+	free(tmp[1]);
+	free(tmp[2]);
+	tmp[1] = ft_strdup("PWD");
+	tmp[2] = ft_strdup(cmd[1]);
 	if (chdir(cmd[1]) == -1)
 	{
 		write(2, "cd: no such file or directory: ", 31);
 		write(2, cmd[1], ft_strlen(cmd[1]));
 		write(2, "\n", 1);
 	}
-	tmp2 = ft_strjoin("OLDPWD=", dir);
-	e[i] = ft_strdup(tmp2);
-	free(tmp);
-	free(tmp2);
-	free(dir);
-	free_tab(name);
 	return (e);
 }
