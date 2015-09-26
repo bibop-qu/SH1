265d9fa8f0ecf6238d2764cfb0acbfb86609f1c7
bibop-qu <lequereb@gmail.com>
2015-09-26 19:03:24 +0200

    new start

--- includes/exec.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef EXEC_H
-# define EXEC_H
-
-void	do_fork(char **cmd, char **e, char **path);
-char	**recup_path(char **e);
-
-#endif
--- includes/tools.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef TOOLS_H
-# define TOOLS_H
-
-int		tab_cmp(char *str, char **tab);
-int		tab_size(char **tab);
-void	free_tab(char **tab);
-
-#endif
