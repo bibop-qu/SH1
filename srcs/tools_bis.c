af2a6701ce7dd684bcde3ecc933d65cfe6b53a45
bibop-qu <lequereb@gmail.com>
2016-02-04 13:21:03 +0100

    tild

--- srcs/tools_bis.c
+++ srcs/tools_bis.c
@@ -14,16 +14,48 @@
 #include "libft.h"
 #include "tools_env.h"
 
+char	*ft_rep(char *line, char *home)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*result;
+	
+	i = 0;
+	j = 0;
+	k = 0;
+	while (line[i] && line[i] != '~')
+		i++;
+	result = (char*)malloc(sizeof(char) * i + ft_strlen(home));
+	while (j < i)
+	{
+		result[j] = line[j];
+		j++;
+	}
+	while (home[k])
+	{
+		result[j] = home[k];
+		j++;
+		k++;
+	}
+	while (line[i])
+	{
+		result[j] = line[i];
+		j++;
+		i++;
+	}
+	ft_putendl(result);
+	return (result);
+}
+
 char	*ft_tild(char *line, t_env *e)
 {
 	int		i;
 	char	*home;
-	char	**tab;
 	char	*result;
 	t_env	*tmp;
 
 	i = find_list("HOME", e);
-	tab = ft_strsplit(line, '~');
 	tmp = e;
 	while (i > 1)
 	{
@@ -32,12 +64,11 @@ char	*ft_tild(char *line, t_env *e)
 	}
 	home = ft_strdup(tmp->value);
 	i = 0;
-	result = ft_strjoin(tab[i], home);
-	while (tab[i])
+	result = ft_strdup(line);
+	while (ft_strchr(result, '~'))
 	{
-		if (i != 0 && tab[++i])
-			result = ft_strjoin(tab[i], home);
-		i++;
+		result = ft_rep(line, home);
 	}
+	ft_putendl(result);
 	return (result);
 }
